7c51600c7fbac49a46ebe3fca0821475913a54c8
TranspoLord <ascatlover9@gmail.com>
2023-07-15 17:22:44 -0500

    Initial creation. Connection of SQL database

---
+++ BookStore/Data/AppDatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookStore/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        [Key] // I know its redundant but I like to be explicit
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+
+    }
+}
