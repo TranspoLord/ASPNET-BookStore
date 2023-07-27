dafda7b20dbd509fb96a436c7bd013d707b09404
TranspoLord <ascatlover9@gmail.com>
2023-07-27 12:02:45 -0500

    Going over what Razor pages are

---
+++ Bookstore_RazorTemp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore_RazorTemp.Models
+{
+    public class Category
+    {
+        [Key] // I know its redundant but I like to be explicit
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
